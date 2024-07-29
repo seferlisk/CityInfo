afa1b8548183c297b975da5518f36ca6a3fb2ef1
seferlisk <seferlisk@gmail.com>
2024-07-29 16:57:11 +0300

    registered API services on the container

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1 , Name = "New York City"},
+                    new {id = 2 , Name = "Antwerp"},
+                });
+        }
+
+        []
+    }
+}
